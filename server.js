a3b78a4ac75aa0a5c217339842ee3f669fdb6495
Andrew Keiser <webdev410@gmail.com>
2021-09-23 13:39:26 -0700

    add workout is working

--- server.js
+++ server.js
@@ -20,6 +20,36 @@ app.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname + "/public/stats.html"));
 });
 
+app.post("/api/workouts", ({ body }, res) => {
+    console.log(body)
+    db.Workout.create(body)
+        .then(({ _id }) => db.Workout.findOneAndUpdate({},
+            { $push: { exercises: _id } }, { new: true }))
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+app.put("/api/workouts/:id", function (req, res) {
+    let id = req.params.id;
+    db.Workout.findOneAndUpdate(
+        { _id: id },
+        { $push: { exercises: req.body } },
+        function (error, success) {
+            if (error) {
+                console.log(error);
+            } else {
+                res.send(success);
+            }
+        }
+    );
+});
+
+
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
 
 
