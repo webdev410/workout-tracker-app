deec5eb1832a2cbe6e6afa5239dd6d7b4d5bfce5
Andrew Keiser <webdev410@gmail.com>
2021-09-25 12:19:42 -0700

    heroku

--- models/Workout.js
+++ models/Workout.js
@@ -3,36 +3,35 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    day: {
-        type: Date,
-        default: Date.now
-    },
-    exercises: [
-        {
-            type: {
-                type: String,
-
-            },
-            name: {
-                type: String
-            },
-            duration: {
-                type: Number
-            },
-            weight: {
-                type: Number
-            },
-            reps: {
-                type: Number
-            },
-            sets: {
-                type: Number
-            },
-            distance: {
-                type: Number
-            },
-        }
-    ]
+	day: {
+		type: Date,
+		default: Date.now,
+	},
+	exercises: [
+		{
+			type: {
+				type: String,
+			},
+			name: {
+				type: String,
+			},
+			duration: {
+				type: Number,
+			},
+			weight: {
+				type: Number,
+			},
+			reps: {
+				type: Number,
+			},
+			sets: {
+				type: Number,
+			},
+			distance: {
+				type: Number,
+			},
+		},
+	],
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
--- server.js
+++ server.js
@@ -3,101 +3,93 @@ const path = require("path");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 
+// start app
 const app = express();
 app.use(logger("dev"));
 
 const PORT = process.env.PORT || 3001;
 const db = require("./models");
-
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+// database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
+});
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb",
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    }
-
-
-);
-
+// routes
 app.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/exercise.html"));
+	res.sendFile(path.join(__dirname + "/public/exercise.html"));
 });
 app.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/stats.html"));
+	res.sendFile(path.join(__dirname + "/public/stats.html"));
 });
-
 app.post("/api/workouts", ({ body }, res) => {
-    console.log(body)
-    db.Workout.create(body)
-        .then(({ _id }) => db.Workout.findOneAndUpdate({},
-            { $push: { exercises: _id } }, { new: true }))
-        .then(dbWorkouts => {
-            res.json(dbWorkouts);
-        })
-        .catch(err => {
-            res.json(err);
-        });
+	console.log(body);
+	db.Workout.create(body)
+		.then(({ _id }) =>
+			db.Workout.findOneAndUpdate(
+				{ $push: { exercises: _id } },
+				{ new: true }
+			)
+		)
+		.then((dbWorkouts) => {
+			res.json(dbWorkouts);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
 });
-
 app.put("/api/workouts/:id", function (req, res) {
-    let id = req.params.id;
-    db.Workout.findOneAndUpdate(
-        { _id: id },
-        { $push: { exercises: req.body } },
-        function (error, success) {
-            if (error) {
-                console.log(error);
-            } else {
-                res.send(success);
-            }
-        }
-    );
+	let id = req.params.id;
+	db.Workout.findOneAndUpdate(
+		{ _id: id },
+		{ $push: { exercises: req.body } },
+		function (error, success) {
+			if (error) {
+				console.log(error);
+			} else {
+				res.send(success);
+			}
+		}
+	);
 });
-
 app.get("/api/workouts", (req, res) => {
-    db.Workout.aggregate([
-        {
-            $addFields: {
-                totalDuration: { $sum: "$exercises.duration" },
-
-            },
-        }
-
-    ])
-        .then(dbWorkouts => {
-            res.json(dbWorkouts);
-        })
-        .catch(err => {
-            res.json(err);
-        });
+	db.Workout.aggregate([
+		{
+			$addFields: {
+				totalDuration: { $sum: "$exercises.duration" },
+			},
+		},
+	])
+		.then((dbWorkouts) => {
+			res.json(dbWorkouts);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
 });
 app.get("/api/workouts/range", function (req, res) {
-
-    db.Workout.aggregate([
-        {
-            $addFields: {
-                totalDuration: { $sum: "$exercises.duration" },
-                dateDifference: {
-                    $subtract: [new Date(), "$day"],
-                },
-            }
-        },
-    ],
-
-    )
-        .then(function (dbWorkouts) {
-            // console.log(dbWorkouts)
-            res.json(dbWorkouts);
-        });
+	db.Workout.aggregate([
+		{
+			$addFields: {
+				totalDuration: { $sum: "$exercises.duration" },
+				dateDifference: {
+					$subtract: [new Date(), "$day"],
+				},
+			},
+		},
+	]).then(function (dbWorkouts) {
+		// console.log(dbWorkouts)
+		res.json(dbWorkouts);
+	});
 });
 
-
+// listener
 app.listen(PORT, () =>
-    console.log(`App listening at http://localhost:${PORT} 🚀`)
+	console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
-
